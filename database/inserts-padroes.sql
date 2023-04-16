f9cefb36cce4f7047505b7b0e873be320259cd49
joaqu1m <joaquim.pires@sptech.school>
2023-04-16 14:03:51 -0300

    começo da database pipefy

--- database/inserts-padroes.sql
+++ database/inserts-padroes.sql
@@ -1,2 +1,7 @@
-insert into canal values (1, "string");
-insert into area values (1, "jardineiro");
+INSERT INTO area (id, nome) VALUES (1, "Jardineiro");
+INSERT INTO area (id, nome) VALUES (2, "Pintor");
+INSERT INTO area (id, nome) VALUES (3, "Eletricista");
+INSERT INTO area (id, nome) VALUES (4, "Encanador");
+INSERT INTO area (id, nome) VALUES (5, "Marceneiro");
+INSERT INTO area (id, nome) VALUES (6, "Montador");
+INSERT INTO area (id, nome) VALUES (7, "Gesseiro");
