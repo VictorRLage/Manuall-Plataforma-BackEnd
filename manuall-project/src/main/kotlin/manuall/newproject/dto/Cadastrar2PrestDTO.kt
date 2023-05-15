d735cb30b08a5ad8b4b436b0f73834b6e4030eb5
deboraflores <debora.flores@sptech.school>
2023-05-14 22:25:56 -0300

    tentando criar endpoint da 2etapaCadastroPrest; não finalizado nem validado ainda

---
+++ manuall-project/src/main/kotlin/manuall/newproject/dto/Cadastrar2PrestDTO.kt
@@ -0,0 +1,12 @@
+package manuall.newproject.dto
+
+data class Cadastrar2PrestDTO (
+    val areaUsuario:Int,
+    //val fotoPerfil:String, // string pq url
+    val orcamentoMin:Double,
+    val orcamentoMax:Double,
+    //val rgFrente:String,
+    //val rgVerso:String
+    ) {
+
+}
