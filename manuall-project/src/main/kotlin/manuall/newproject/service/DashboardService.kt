df4d68177e4717e8e7377114a29dad95a9b6f98b
deboraflores <debora.flores@sptech.school>
2023-05-21 13:09:30 -0300

    criacao de dashController e service, com endpoint de porcentagem de usuarios vindos de cada canal de mkt; testado

---
+++ manuall-project/src/main/kotlin/manuall/newproject/service/DashboardService.kt
@@ -0,0 +1,30 @@
+package manuall.newproject.service
+
+import manuall.newproject.domain.Usuario
+import manuall.newproject.repository.UsuarioRepository
+import org.springframework.stereotype.Service
+import java.text.DecimalFormat
+
+@Service
+class DashboardService(
+    val usuarioRepository: UsuarioRepository
+) {
+
+    fun usuariosCanal(tipoUsuario:Int): List<String> {
+        var usuario = usuarioRepository.countByTipoUsuarioGroupByCanal(tipoUsuario)
+
+        val totalUsuarios = usuario.sum().toDouble()
+        val porcentagem = mutableListOf<String>()
+        for (i in usuario) {
+            val df = DecimalFormat("#.##")
+            porcentagem.add(df.format((i.toDouble() * 100) / totalUsuarios))
+
+        }
+        return porcentagem
+    }
+
+
+    fun pegarRegiao() {
+
+    }
+}
