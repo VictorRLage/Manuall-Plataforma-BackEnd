bd91edcf39987272500c7841f42ac8de639e4394
julia-araripe <julia.lopes@sptech.school>
2023-05-22 11:15:30 -0300

    alterando modelagem baseado no pipefy

--- database/inserts-padroes.sql
+++ database/inserts-padroes.sql
@@ -19,8 +19,8 @@ INSERT INTO usuario
 (`nome`, `email`, `senha`, `cpf`, `telefone`, `orcamento_min`, `orcamento_max`, `descricao`, `presta_aula`, `plano`, `status`, `anexo_pfp`, `acessos`, `tipo_usuario`, `canal`, `area_id`)
 VALUES
 ('Joaquim Gimenes Pires', 'joaquim.pires@sptech.school', '$2a$10$Pfz3ATcOC0jdOt4F36IxGuGBrfRtxovR1LAZkWoolJRdd7xVF.4sS', '12345678901', '11987654321', 500.00, 1000.00, 'Sou um professor experiente', 1, 1, 2, 'https://static-cse.canva.com/blob/759723/DrobotDeanCanva.jpg', 0, 1, 0, 1),
-('Maria Santos', 'maria.santos@example.com', '$2a$10$cBT.xH1jVvoCDdc/SEwaOutmAUlZoTVBsDxawF42yAW136grzNMpa', '98765432109', '11976543210', 1000.00, 2000.00, 'Ofereço aulas particulares', 0, 2, 2, 'https://lh3.googleusercontent.com/D9SY17HT2_KWmh0NPba37PmCyGdu4OvEiNOdHNNy2r58dgxod057sP5nYYbZwW080tFKWlhKrNXKEfbcOsYbfv4M6FqUpJAQfMlOWGad', 0, 2, 1, 3),
-('Ana Oliveira', 'ana.oliveira@example.com', '$2a$10$5ph0RY7U8DKcKkHKJgYjPOToAtW6y0ZfQygJSiR4j2DYeFSKytJni', '76543210987', '11965432109', 1500.00, 3000.00, 'Posso ajudar em diversas disciplinas', 1, 3, 2, 'https://i.pinimg.com/originals/f5/67/bf/f567bfd530f04a3aff832f38844fe5be.jpg', 0, 1, 4, 5),
+('Maria Santos', 'maria.santos@example.com', '$2a$10$cBT.xH1jVvoCDdc/SEwaOutmAUlZoTVBsDxawF42yAW136grzNMpa', '98765432109', '11976543210', 1000.00, 2000.00, 'Ofereço aulas particulares', 0, 2, 2, 'avatar.jpg', 0, 2, 1, 3),
+('Ana Oliveira', 'ana.oliveira@example.com', '$2a$10$5ph0RY7U8DKcKkHKJgYjPOToAtW6y0ZfQygJSiR4j2DYeFSKytJni', '76543210987', '11965432109', 1500.00, 3000.00, 'Posso ajudar em diversas disciplinas', 1, 3, 2, 'brebee.jpg', 0, 1, 4, 5),
 ('Fernanda Souza', 'fernanda.souza@example.com', '$2a$10$IMisr8TdWydcgH22StC/lutUcdtDmqYVDWxyJmZmQX8Tp.nraC5dq', '23456789012', '11912345678', 800.00, 1500.00, 'Professora de Matemática', 1, 3, 2, 'perfil.jpg', 0, 1, 3, 2),
 ('Rafaela Lima', 'rafaela.lima@example.com', '$2a$10$NFvy/hCRZkaEMiE468y1quooIfRTVWPJEAAE2772.iry.TCCaYO3a', '34567890123', '11923456789', 1200.00, 2000.00, 'Especialista em Ciências', 0, 2, 2, 'foto.png', 0, 2, 1, 6),
 ('Carlos Mendes', 'carlos.mendes@example.com', '$2a$10$UT.ocrWrVawMkbqVc/6RN.NALwJOcuKIXupneVOZ.nlnWX3KBEKQy', '45678901234', '11934567890', 1500.00, 3000.00, 'Aulas de Inglês para todos os níveis', 1, 1, 2, 'avatar.jpg', 0, 1, 4, 7),
@@ -56,3 +56,5 @@ select * from usuario;
 select * from servico;
 select * from solicitacao;
 select * from chat;
+
+select * from prospect;
