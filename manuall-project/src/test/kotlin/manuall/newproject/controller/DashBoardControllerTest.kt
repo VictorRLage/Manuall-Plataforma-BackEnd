b00cff136b5673bd2f0d9d69cf8d896c96d56617
deboraflores <debora.flores@sptech.school>
2023-05-23 14:36:50 -0300

    tentativa de início de teste

---
+++ manuall-project/src/test/kotlin/manuall/newproject/controller/DashBoardControllerTest.kt
@@ -0,0 +1,13 @@
+package manuall.newproject.controller
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
+
+class DashBoardControllerTest {
+
+
+}
