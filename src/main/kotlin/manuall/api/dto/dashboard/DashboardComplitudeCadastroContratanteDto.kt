b62d1e48424aad7f5079fd5a74eeb26303fdd2aa
joaqu1m <joaquim.pires@sptech.school>
2023-10-19 21:13:18 -0300

    usuarioservice avaliacao

--- src/main/kotlin/manuall/api/dto/dashboard/DashboardComplitudeCadastroContratanteDto.kt
+++
@@ -1,6 +0,0 @@
-package manuall.api.dto.dashboard
-
-data class DashboardComplitudeCadastroContratanteDto (
-    val `1`: Int,
-    val `2`: Int
-)
--- src/main/kotlin/manuall/api/dto/dashboard/PegarRegiaoDto.kt
+++
@@ -1,7 +0,0 @@
-package manuall.api.dto.dashboard
-
-data class PegarRegiaoDto(
-    val cidade:String,
-    val estado:String,
-    val contador: Long
-)
